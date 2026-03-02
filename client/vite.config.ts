969481e914d7b1f3ba8373f85afde986b8776011
Henri <henriknight@hotmail.co.uk>
2026-03-02 12:48:21 +0000

    feat: Add startup handler for main Electron entry in Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     electron([
       {
         entry: "electron/main.ts",
+        onstart(args) {
+          args.startup();
+        },
         vite: {
           build: {
             outDir: "dist-electron",
